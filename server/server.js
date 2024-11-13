e2754034cf0718af104c1e6da25c81934fff9441
FahadYounas <fahadyounas10@gmail.com>
2024-11-13 13:47:19 +0500

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,20 @@
+// server.js
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB connected"))
+  .catch((error) => console.log(error));
+
+// Default route
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
